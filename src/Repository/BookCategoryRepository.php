f5501f76d8ab2a7ea050b58995fc786b6749feec
kolomdima111 <kolomdima111@gmail.com>
2022-05-02 14:02:10 +0200

    First commit

---
+++ src/Repository/BookCategoryRepository.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\BookCategory;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\OptimisticLockException;
+use Doctrine\ORM\ORMException;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<BookCategory>
+ *
+ * @method BookCategory|null find($id, $lockMode = null, $lockVersion = null)
+ * @method BookCategory|null findOneBy(array $criteria, array $orderBy = null)
+ * @method BookCategory[]    findAll()
+ * @method BookCategory[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class BookCategoryRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, BookCategory::class);
+    }
+
+    /**
+     * @throws ORMException
+     * @throws OptimisticLockException
+     */
+    public function add(BookCategory $entity, bool $flush = true): void
+    {
+        $this->_em->persist($entity);
+        if ($flush) {
+            $this->_em->flush();
+        }
+    }
+
+    /**
+     * @throws ORMException
+     * @throws OptimisticLockException
+     */
+    public function remove(BookCategory $entity, bool $flush = true): void
+    {
+        $this->_em->remove($entity);
+        if ($flush) {
+            $this->_em->flush();
+        }
+    }
+}
---
+++ src/Service/BookCategoryService.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Service;
+
+use App\Repository\BookCategoryRepository;
+
+class BookCategoryService
+{
+    public function __construct(private BookCategoryRepository $bookCategoryRepository)
+    {
+    }
+}
